7a7ccdc04d4bcad0a45aa8c719c50c2ddd57cd0f
liubo <2281075105@qq.com>
2019-07-03 11:49:34 +0800

    01_测试tag

--- NOTE.h
+++ NOTE.h
@@ -9,3 +9,15 @@ git commit -m '提交的描述内容'
 git remote add origin https://github.com/lb2281075105/02.LBMiniprogram.git
 
 git push -u origin master
+
+// ---------------------------------------
+// 添加tag
+git add .
+git commit -m '01_知识点1'
+git tag 01_知识点1
+// git tag (查看项目中有多少个tag)
+// git log (查看项目提交信息)
+// git status (查看现在仓库的提交状态)
+
+// 强制回退到版本号的那个版本
+git reset --hard 版本号
